2b852c09e8c71bf283a164f6f9657e9ffcfef7db
nissy-dev <nd.12021218@gmail.com>
2022-07-07 21:25:39 +0900

    feat: support feather icons

--- packages/lib/src/icons.rs
+++ packages/lib/src/icons.rs
@@ -2,6 +2,7 @@ pub mod bs_icons;
 pub mod fa_brands_icons;
 pub mod fa_regular_icons;
 pub mod fa_solid_icons;
+pub mod fi_icons;
 pub mod go_icons;
 pub mod hi_outline_icons;
 pub mod hi_solid_icons;
